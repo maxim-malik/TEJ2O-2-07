0a03e5b37f015d02272246dabf9e46e63b24c735
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-10-02 20:46:46 -0400

    Added simple number counter

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Max
+ * Created on: Oct 2025
+ * This program functions as a simple number counter
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
+
+let clicks = 0 //creates "clicks" variable
+
+input.onButtonPressed(Button.A, () => { 
+basic.clearScreen(); //clears screen
+clicks = clicks + 1 //adds 1 to clicks
+basic.showNumber(clicks) //displays value of clicks
+})
+
+input.onButtonPressed(Button.B, () => {
+clicks = 0 //resets the number of clicks
+basic.showNumber(clicks) //displays value of clicks
+})
